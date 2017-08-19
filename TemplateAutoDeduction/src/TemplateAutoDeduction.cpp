00840a5d6c2067ac633ef3e2a79d79d2670fdb3f
subbu <subbu.klce@outlook.com>
2017-08-19 23:19:17 +0530

    new projects created for Template auto deduction

---
+++ TemplateAutoDeduction/src/TemplateAutoDeduction.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TemplateAutoDeduction.cpp
+// Author      : Subrahmanyam Gundimeda
+// Version     :
+// Copyright   : Free for educaional purpose.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Template Auto Deduction!!!" << endl; // prints !!!Template Auto Deduction!!!
+	return 0;
+}
