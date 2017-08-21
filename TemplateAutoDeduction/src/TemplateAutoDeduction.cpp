96c26a11ad0a391781f00422963a1decff1f975b
subbu <subbu.klce@outlook.com>
2017-08-21 17:28:50 +0000

    Adding changes for TemplateAuto Deduction Excercise

--- TemplateAutoDeduction/src/TemplateAutoDeduction.cpp
+++ TemplateAutoDeduction/src/TemplateAutoDeduction.cpp
@@ -5,11 +5,29 @@
 // Copyright   : Free for educaional purpose.
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
-#include <iostream>
+#include "TemplateAutoDeduction.h"
 using namespace std;
 
-int main() {
-	cout << "!!!Template Auto Deduction!!!" << endl; // prints !!!Template Auto Deduction!!!
-	return 0;
+bool Template_fucntion_selector(template_selector type_selector){
+
+	switch(type_selector){
+	case template_selector::passbyvalue:
+	{
+		int x = 10;
+		const int y = x;
+		const int & z = x;
+
+		myfunction(12);
+		myfunction(y);
+		myfunction(z);
+
+	}
+			break;
+	case template_selector::passbyreference_pointer:
+			break;
+	case template_selector::passbyuniversal:
+			break;
+
+	}
+return true;
 }
